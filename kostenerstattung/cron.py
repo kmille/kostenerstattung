3cdf75edddcde36ebcdbe876eff8d6115a16dd5b
kmille <6470600+kmille@users.noreply.github.com>
2026-05-07 10:27:07 +0200

    Restructure cron.py

--- kostenerstattung/cron.py
+++ kostenerstattung/cron.py
@@ -31,14 +31,35 @@ def update_ticket(erstattung, webling_booking_url):
     logger.debug("Updated Erstattung in ticket")
 
 
-def cron():
-    def update_database():
-        erstattung.state = ErstattungsState.DONE
-        erstattung.entrygroup_id = entrygroup_id
-        erstattung.buchungs_id = booking_id
-        db.session.commit()
-        logger.debug("Updated Erstattung in db")
+def update_database(erstattung, entrygroup_id, booking_id):
+    erstattung.state = ErstattungsState.DONE
+    erstattung.entrygroup_id = entrygroup_id
+    erstattung.buchungs_id = booking_id
+    db.session.commit()
+    logger.debug("Updated Erstattung in db")
 
+
+def book_kobu_ueberweisung(erstattung, lastschrift_properties, lastschrift_id):
+    date_bank = datetime.strptime(lastschrift_properties["made_on"], "%Y-%m-%d").date()
+    if date_bank != erstattung.paid_at.date():
+        logger.warning(f" Lastschrift: Date mismatch (bank={date_bank}, erstattet={erstattung.paid_at.date()})")
+        if abs((erstattung.paid_at.date() - date_bank).days) < 4:
+            erstattung.paid_at = datetime(date_bank.year, date_bank.month, date_bank.day)
+            logger.info(" Ignoring date mismatch as difference is below 4 day threshold")
+        else:
+            return
+
+    belege = config["zammad_api"].get_concatenated_attachments_from_ticket(erstattung.ticket_number)
+    entrygroup_id = config["webling_api"].create_buchung(erstattung, lastschrift_id, belege)
+    booking_id = config["webling_api"].get_buchungs_id(entrygroup_id)
+
+    webling_booking_url = f"{config['webling']['base_url']}/admin#/accounting/{config['webling']['default_buchungsperiode_id']}/entrygroup/:entrygroup/editor/{entrygroup_id}"
+    logger.info(f" Die Erstatung {erstattung.id} wurde in Webling verbucht (Buchung #{booking_id}, {webling_booking_url})")
+    update_ticket(erstattung, webling_booking_url)
+    update_database(erstattung, entrygroup_id, booking_id)
+
+
+def cron():
     logger.info("Starting cron routine to book Webling Erstattungen paid in KoBu")
     unbooked_lastschriften = config["webling_api"].get_unverbuchte_lastschriften()
     with app.app_context():
@@ -52,28 +73,12 @@ def cron():
                 lastschrift_betrag = lastschrift_properties["amount"]
                 if lastschrift_betrag > 0:
                     continue
+                # handle Kobu Überweisung
                 if lastschrift_properties["description"].startswith(erstattung.verwendungszweck) and \
                         lastschrift_iban == erstattung.iban.replace(" ", "") and \
                         abs(float(lastschrift_betrag)) == erstattung.betrag:
                     logger.info(f" Match {erstattung} (#{erstattung.ticket_number}) with '{lastschrift_properties['description']}'")
-
-                    date_bank = datetime.strptime(lastschrift_properties["made_on"], "%Y-%m-%d").date()
-                    if date_bank != erstattung.paid_at.date():
-                        logger.warning(f" Lastschrift: Date mismatch (bank={date_bank}, erstattet={erstattung.paid_at.date()})")
-                        if abs((erstattung.paid_at.date() - date_bank).days) < 4:
-                            erstattung.paid_at = datetime(date_bank.year, date_bank.month, date_bank.day)
-                            logger.info(" Ignoring date mismatch as difference is below 4 day threshold")
-                        else:
-                            continue
-
-                    belege = config["zammad_api"].get_concatenated_attachments_from_ticket(erstattung.ticket_number)
-                    entrygroup_id = config["webling_api"].create_buchung(erstattung, lastschrift["id"], belege)
-                    booking_id = config["webling_api"].get_buchungs_id(entrygroup_id)
-
-                    webling_booking_url = f"{config['webling']['base_url']}/admin#/accounting/{config['webling']['default_buchungsperiode_id']}/entrygroup/:entrygroup/editor/{entrygroup_id}"
-                    logger.info(f" Die Erstatung {erstattung.id} wurde in Webling verbucht (Buchung #{booking_id}, {webling_booking_url})")
-                    update_ticket(erstattung, webling_booking_url)
-                    update_database()
+                    #book_kobu_ueberweisung(erstattung, lastschrift_properties, lastschrift["id"])
 
 
 if __name__ == '__main__':
